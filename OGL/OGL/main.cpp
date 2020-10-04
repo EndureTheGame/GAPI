87a1a518cd293756828c0981f74210dbdf621e2e
EndureTheGame <jose110997@gmail.com>
2020-10-04 09:29:42 -0500

    Files Commit

---
+++ OGL/OGL/main.cpp
@@ -0,0 +1,18 @@
+#pragma once 
+
+#include <iostream>
+#include <String>
+
+
+
+int main()
+{
+
+	std::string string = "Hello World";
+
+	std::cout << string << std::endl;
+	std::cin.get();
+
+	return 0;
+
+}
